6d14085cc6f8f5063fce7fbefae8eceec50227c2
Chana <hsklar@student.touro.edu>
2017-10-29 13:39:51 -0400

    Missing code from past 2 commits

--- src/Betting.java
+++ src/Betting.java
@@ -1,4 +1,29 @@
 
 public class Betting {
-
+	
+	private double currentBalance;
+	private IRandomValueGenerator gen;
+	private int minBalance;
+	
+	public Betting (int minBalance, IRandomValueGenerator gen){
+		this.minBalance=minBalance;
+		this.gen=gen;
+		this.currentBalance=0;
+	}
+	
+	public double getCurrentBalance() {
+		 return currentBalance;
+	}
+	
+	public void addMoney(int amnt){
+		throw new RuntimeException();
+	}
+	//returns true for a win, false for a loss
+	public boolean betOnANumber(int amnt, int min, int max, int selectedNumber) {
+		throw new RuntimeException();
+	}
+			
+	public boolean betOnProbability(int amnt, double p){ 
+		throw new RuntimeException();
+	}
 }
--- src/MockGenerator.java
+++ src/MockGenerator.java
@@ -1,4 +1,17 @@
 
-public class MockGenerator {
+public class MockGenerator implements IRandomValueGenerator{
+	
+	private int value;
 
+	public MockGenerator(){
+		this.value=0;
+	}
+	
+	public int getValue(int lowest, int highest){
+		return value;
+	}
+	
+	public void setValue(int value){
+		this.value=value;
+	}
 }
